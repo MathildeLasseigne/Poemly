4fb5d2c0ce6ec1e2f11f1da3e43aed96efc716b2
MathildeLasseigne <mathilde.lasseigne@gmail.com>
2021-09-25 19:40:41 +0200

    Finalize karaoke colorizer and test the parse methodes

--- src/prototypeGame/widgets/Karaoke/KaraokeColorizer.java
+++ src/prototypeGame/widgets/Karaoke/KaraokeColorizer.java
@@ -9,22 +9,17 @@ import javafx.scene.text.Text;
 import java.util.ArrayList;
 
 /**
+ * Colorize a text with 3 colors. A separator char has a specific color, and separate the 2 other parts of the text
  * From https://stackoverflow.com/questions/15081892/javafx-text-multi-word-colorization
  */
 public class KaraokeColorizer {
 
+    /**The text to colorize*/
+    private final ArrayList<String> text;
 
-
-
-    private String[] wordList = {};
 
     /**
-     * Line of words. Words are in an HBox to prevent warping in the middle of a word
-     */
-    private ArrayList<HBox> wordOutList = new ArrayList<>();
-
-    /**
-     * List of all the lines. Lines are in flowpane to allow warping
+     * List of all the rendered lines. Lines are in FlowPane to allow warping
      */
     private ArrayList<FlowPane> lineOutList = new ArrayList<>();
 
@@ -33,13 +28,23 @@ public class KaraokeColorizer {
      */
     private int separatorIdx = -1;
 
+
+    private final TextRender renderBefore, renderSeparator, renderAfter;
+
     /**
      * Colorize a text with 3 colors. A separator char has a specific color, and separate the 2 other parts of the text
      * <br/> The index of the separator begin at -1, meaning all the text is colored with the 3rd color
      * @param text the text to colorize
+     * @param renderBefore render before the separator
+     * @param renderSeparator render of the separator
+     * @param renderAfter render after the separator (if separator not initialized, will be the render for the whole text)
      */
-    public KaraokeColorizer(ArrayList<String> text){
+    public KaraokeColorizer(ArrayList<String> text, TextRender renderBefore, TextRender renderSeparator, TextRender renderAfter){
 
+        this.text = text;
+        this.renderBefore = renderBefore;
+        this.renderSeparator = renderSeparator;
+        this.renderAfter = renderAfter;
     }
 
     public void setSeparatorIdx(int separatorIdx) {
@@ -52,47 +57,82 @@ public class KaraokeColorizer {
 
     /**
      * Return the text rendered. Each row of the arraylist correspond to a line.
-     * @return
+     * @return all the rendered text in FlowPanes
      */
-    public ArrayList<FlowPane> getRenderText() {
+    public ArrayList<FlowPane> getRenderedText() {
+        colorize();
         return lineOutList;
     }
 
-    public FlowPane parseInputToArray(String input) {
-        wordList = input.trim().split("[ ]+");
 
 
-        return colorize();
+    /**
+     * Parse the string (the line) into words. All spaces and dots are kept but the String is trimmed
+     * <br/> The following strings
+     * <ul>
+     *     <li>"Hello, my name is Blanc! I'm a student. Can you help me? Thanks."</li>
+     *     <li>"      Hello, my name is Blanc! I'm a student. Can you help me? Thanks.     "</li>
+     * </ul>
+     *  will be parsed the same way :
+     * <br/> "|Hello, |my |name |is |Blanc!| |I'm |a |student.| |Can |you |help |me?| |Thanks.|"
+     * @param input the string to parse
+     * @return the words of the input as an array
+     * @see String#split(String)
+     * @see String#trim()
+     */
+    public static String[] parseInputToWordsArray(String input) {
+        return input.trim().split("(?<= )|(?<=\\.)|(?<=\\?)|(?<=!)");
     }
 
     /**
-     * Parse the string into words
-     * @param input
+     * Parse the string into characters
+     * @param word the word to extract
+     * @return the characters of the word as a char array
+     * @see String#charAt(int)
      */
-    public void parseInputToArrayToWords(String input) {
-        wordList = input.trim().split("(?<= )|(?<=\\.)");
-    }
-
-    /**
-     * Parse the string into words
-     * @param word
-     */
-    public char[] parseWordToCharacterArray(String word) {
+    public static char[] parseInputToCharacterArray(String word) {
         char[] charList = new char[word.length()];
 
         for(int i = 0; i<word.length(); i++) {
             charList[i] = word.charAt(i);
             // access each character
         }
-        //Text text = new Text();
-        //text.setText(String.valueOf(charList[0]));
         return charList;
     }
 
-    public FlowPane colorize() {
+    /**
+     * Compute the render for each character output it into the lineOutList var
+     * @see KaraokeColorizer#lineOutList
+     */
+    public void colorize() {
 
-        ArrayList<Text> textChunks = new ArrayList<>();
-        FlowPane bundle = new FlowPane();
+        for(String str : this.text){
+
+            String[] words = parseInputToWordsArray(str);
+            FlowPane line = new FlowPane(); //Line box
+            int currentIdx = 0; //To check the separator
+            for(int i = 0; i< words.length; i++){
+                char[] character = parseInputToCharacterArray(words[i]);
+                HBox word = new HBox(); //Word box. Words are in an HBox to prevent warping in the middle of a word
+
+                for(int j = 0; j< character.length; j++){
+                    Text c = null;
+                    if(currentIdx < this.separatorIdx){
+                        c = this.renderBefore.customize(String.valueOf(character[i]));
+                    } else if(currentIdx == this.separatorIdx){
+                        c = this.renderSeparator.customize(String.valueOf(character[i]));
+                    } else if(currentIdx > this.separatorIdx){
+                        c = this.renderAfter.customize(String.valueOf(character[i]));
+                    }
+
+                    word.getChildren().add(c);
+                    currentIdx++;
+                }
+                line.getChildren().add(word);
+
+            }
+            this.lineOutList.add(line);
+        }
             /*
             //Todo: use regex to check for valid words
             for (String word : wordList) {
@@ -114,27 +154,17 @@ public class KaraokeColorizer {
 
              */
 
-        bundle.getChildren().addAll(textChunks);
-        return bundle;
-    }
-
-    public Text customize(String word, Color color) {
-        Text text = new Text("word");
-        text.setFill(color);
-        return text;
-    }
-
-    public Text customize(String word, Color color, Font font) {
-        Text txt = customize(word, color);
-        txt.setFont(font);
-        return txt;
     }
 
 
-    public class TextRender {
+    /**
+     * Render the text depending on the specification.
+     * <br/> Use this class to render multiple texts the same way
+     */
+    public static class TextRender {
 
-        private Color color;
-        private Font font;
+        private final Color color;
+        private final Font font;
 
         public TextRender(Color color, Font font){
 
@@ -142,11 +172,36 @@ public class KaraokeColorizer {
             this.font = font;
         }
 
+        public TextRender(Color color){
+            this(color, null);
+        }
+
+        public TextRender(Font font){
+            this(null, font);
+        }
+
+        /**
+         * Add this TextRenderer properties to the given text
+         * @param text a Text to customize
+         */
+        public void customize(Text text){
+            Text renderedText = text;
+            if(this.color != null){
+                renderedText.setFill(this.color);
+            }
+            if(this.font != null){
+                renderedText.setFont(this.font);
+            }
+        }
+
+        /**
+         * Create a Text from the given String and add it this TextRenderer properties
+         * @param text the text property of the return Text
+         * @return a Text object with the parameter text as text property, customized
+         */
         public Text customize(String text){
             Text renderedText = new Text(text);
-            if(this.color != null){
-                //renderedText.se
-            }
+            customize(renderedText);
             return renderedText;
         }
 
---
+++ src/prototypeGame/widgets/Karaoke/TestKaraoke.java
@@ -0,0 +1,28 @@
+package prototypeGame.widgets.Karaoke;
+
+public class TestKaraoke {
+
+    public static void main(String[] args){
+        String s = " Hello, my name is Blanc! I'm a student. " +
+                "I am new here, so please help me adapt. Can you help me? Thanks." ;
+
+        String[] out = KaraokeColorizer.parseInputToWordsArray(s);
+        for(int i = 0; i< out.length; i++){
+            System.out.print(out[i]+ ";");
+        }
+
+        System.out.println("\nSecond test");
+
+        String[] words = s.split("\\s+");
+        for(int i = 0; i< words.length; i++){
+            System.out.print(words[i]+ ";");
+        }
+
+        System.out.println("\nThird test");
+
+        String[] words2 = s.trim().split("[ ]+");
+        for(int i = 0; i< words2.length; i++){
+            System.out.print(words2[i]+ ";");
+        }
+    }
+}
