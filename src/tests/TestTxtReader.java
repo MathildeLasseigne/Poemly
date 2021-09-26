ef97ef4c876fc08ca5f3a8bb4929d88dea3b62c5
MathildeLasseigne <mathilde.lasseigne@gmail.com>
2021-09-26 13:00:41 +0200

    Test from the actual FileListManager class

--- src/tests/TestTxtReader.java
+++ src/tests/TestTxtReader.java
@@ -1,5 +1,7 @@
 package tests;
 
+import widgets.tools.FileListManager;
+
 import java.util.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
@@ -10,33 +12,11 @@ public class TestTxtReader {
 
 
     public static void main(String[] args){
-        List l = ReadFileIntoList.readFileInList("src/assets/tests/testReader.txt");
+        List l = FileListManager.readFileInList("src/assets/tests/testReader.txt");
 
         Iterator<String> itr = l.iterator();
         while (itr.hasNext())
-            System.out.println(itr.next());
+            System.out.print(itr.next());
     }
 
-
-    static class ReadFileIntoList
-    {
-        public static List<String> readFileInList(String fileName)
-        {
-
-            List<String> lines = Collections.emptyList();
-            try
-            {
-                lines =
-                        Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-            }
-
-            catch (IOException e)
-            {
-
-                // do something
-                e.printStackTrace();
-            }
-            return lines;
-        }
-    }
 }
