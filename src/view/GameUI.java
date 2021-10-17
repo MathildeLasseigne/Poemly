313cfedc7faa62e48ff4d80013847659e9539b74
cateau <cateau-oonk@hotmail.com>
2021-10-17 10:00:22 +0200

    remove fx:controllers from FXML files

--- src/view/GameUI.java
+++ src/view/GameUI.java
@@ -5,52 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import prototypeInterface.controller.InterfaceController;
 
 import javax.swing.*;
 import java.io.IOException;
 
 public class GameUI extends Pane {
 
+    public InterfaceController interfaceController;
+
+
     private Game game;
 
     public GameUI(Game game){
         this.game = game;
+        //this.interfaceController = new InterfaceController();
+        //this.getChildren().add();
     }
 
-
-
-    public Parent setMenu()throws IOException  {
-        Parent menuPanel = null;
-            menuPanel = FXMLLoader.load((getClass().getResource("prototypeInterface/view/Menu.fxml")));
-            return menuPanel;
-        }
-
-    public Parent setHelp()throws IOException   {
-        Parent helpPanel = null;
-        helpPanel = FXMLLoader.load((getClass().getResource("prototypeInterface/view/Menu.fxml")));
-        return helpPanel;
-    }
-
-
-
-    public Scene setSchool() throws IOException {
-        Parent scoreScreen = FXMLLoader.load(getClass().getClassLoader().getResource("prototypeInterface/view/School.fxml"));
-        Scene scoreScene = new Scene(scoreScreen);
-        return scoreScene;
-    }
-    public Scene setCity() throws IOException {
-        Parent scoreScreen = FXMLLoader.load(getClass().getClassLoader().getResource("prototypeInterface/view/City.fxml"));
-        Scene scoreScene = new Scene(scoreScreen);
-        return scoreScene;
-    }
-    public Scene setBeach() throws IOException {
-        Parent scoreScreen = FXMLLoader.load(getClass().getClassLoader().getResource("prototypeInterface/view/Beach.fxml"));
-        Scene scoreScene = new Scene(scoreScreen);
-        return scoreScene;
-    }
-    public Scene setGameOver() throws IOException {
-        Parent scoreScreen = FXMLLoader.load(getClass().getClassLoader().getResource("prototypeInterface/view/GameOver.fxml"));
-        Scene scoreScene = new Scene(scoreScreen);
-        return scoreScene;
-    }
 }
